b4ee05e6d1d2a747c218d40abcfb01b7acbf805a
Kevin <kkornafel82@yahoo.com>
2015-11-02 18:31:17 -0800

    collaborators progress

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,5 +1,25 @@
 class CollaboratorsController < ApplicationController
 
-def create
-  
+  def create
+   @collaborator = Collaborator.new(params.require(:collaborator).permit(:id))
+    if @collaborator.save
+    flash[:notice] = "Collaborator was saved."
+    redirect_to wiki_path(wiki.id)
+    else
+     flash[:error] = "There was an error saving the collaborator. Please try again."
+     render :new
+    end
+  end
+
+  def destroy
+    @collaborator = Collaborator.find(params[:id])
+      if @collaborator.destroy
+        flash[:notice] = "Collaborator was deleted successfully."
+        redirect_to wiki_path(wiki.id)
+      else
+        flash[:error] = "There was an error deleting the collaborator."
+        render :show
+      end
+  end
+
 end
